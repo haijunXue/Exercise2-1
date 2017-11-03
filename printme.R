d9131369cb66419de1e35c8273bcbbf2a29e49a7
haijunXue <xuehaijun793g@gmail.com>
2017-11-03 09:33:50 +0100

    commit

--- printme.R
+++ printme.R
@@ -18,4 +18,7 @@ printer(name,eyecolor,meal)
 # create them from the shell
 #----------4.3.4
 #git checkout -b testing
-#git push -u origin testing
+#git push -u origin testing#git push -u origin testing
+
+#--------4.7
+#check the differencies between master and branch in the command line via git diff.
